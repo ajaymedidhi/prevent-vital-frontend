dbd699e1546993cb64d4434f0cc68cff724bdff3
ajaymedidhi <ajaymedidhi858@gmail.com>
2026-02-01 22:23:19 +0530

    Deploy: Global Scale & Hero Padding Fixes

--- src/components/homepage/HeroSection.tsx
+++ src/components/homepage/HeroSection.tsx
@@ -77,7 +77,7 @@ const HeroSection = ({ className = '' }: HeroSectionProps) => {
             </div>
 
             {/* Content */}
-            <div className="relative z-20 container mx-auto px-4 h-full flex items-center pt-24 md:pt-0">
+            <div className="relative z-20 container mx-auto px-4 h-full flex flex-col justify-center md:justify-start pt-24 md:pt-20">
                 <AnimatePresence mode="wait">
                     <motion.div
                         key={currentSlide}
